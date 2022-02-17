c53361c1602bd73c899d312aab7b77f000b68e94
Abdul <abdulmomin956@gmail.com>
2022-02-18 04:50:28 +0600

    finishing

--- script/index.js
+++ script/index.js
@@ -5,7 +5,6 @@ function getInputValue(number) {
 }
 function checkInput(value1) {
     if (value1.indexOf('income') != -1) {
-        debugger;
         return getInputValue(value1);
     }
     else {
@@ -15,19 +14,115 @@ function checkInput(value1) {
 function subtraction(value1, value2, result) {
     const num1 = checkInput(value1);
     const num2 = document.getElementById(value2).innerText;
+
     document.getElementById(result).innerText = num1 - num2;
 }
+
+// calculate button 
 document.getElementById('calculate').addEventListener('click', function () {
     const food = getInputValue("food");
     const rent = getInputValue("rent");
     const cloth = getInputValue("cloth");
-    document.getElementById('total-expenses').innerText = food + rent + cloth;
-    subtraction('income', 'total-expenses', 'balance');
+    const income = getInputValue('income');
+    if (isNaN(food) || isNaN(rent) || isNaN(cloth) || isNaN(income)) {
+        document.getElementById('check-number').style.display = 'block';
+    }
+    else {
+        if (food < 0 || rent < 0 || cloth < 0 || income < 0) {
+            debugger;
+            document.getElementById('check-positive').style.display = 'block';
+        }
+        else {
+            const expense = document.getElementById('total-expenses').innerText = food + rent + cloth;
+            if (expense > income) {
+                document.getElementById('total-expenses').innerText = '00';
+                document.getElementById('check-expense').style.display = 'block';
+            }
+            else {
+                subtraction('income', 'total-expenses', 'balance');
+                document.getElementById('check-number').style.display = 'none';
+                document.getElementById('check-positive').style.display = 'none';
+                document.getElementById('check-expense').style.display = 'none';
+            }
+
+        }
+
+    }
 });
 
+
+// save button 
 document.getElementById('save').addEventListener('click', function () {
     const parcentage = getInputValue("percentage");
     const income = getInputValue('income');
-    document.getElementById('saving-amount').innerText = income * parcentage / 100;
-    subtraction('balance', 'saving-amount', 'last-balance');
+    const balance = document.getElementById('balance').innerText;
+    if (isNaN(income) || isNaN(parcentage)) {
+        document.getElementById('saving').style.display = 'block';
+    }
+    else {
+        if (balance == '00') {
+            document.getElementById('balance-first').style.display = 'block';
+            document.getElementById('saving').style.display = 'none';
+        }
+        else {
+            document.getElementById('balance-first').style.display = 'none';
+            const saving = document.getElementById('saving-amount').innerText = income * parcentage / 100;
+            if (saving > balance) {
+                document.getElementById('saving-amount').innerText = '00'
+                document.getElementById('last-balance').innerText = '00'
+                document.getElementById('saving-check').style.display = 'block';
+            }
+            else {
+                subtraction('balance', 'saving-amount', 'last-balance');
+                document.getElementById('saving-check').style.display = 'none';
+            }
+
+
+        }
+
+    }
+
 });
+
+
+// form inputfield color change 
+function borderColorChange(tag) {
+    if (!isNaN(tag.value) && tag.value >= 0 || tag.value == '') {
+        tag.style.border = '1px solid ';
+        tag.style.outline = '1px solid ';
+    }
+    else {
+
+        tag.style.border = '1px solid red';
+        tag.style.outline = '1px solid red';
+    }
+}
+
+const formParent = document.getElementById('form');
+
+formParent.addEventListener('focus', function (inputValue) {
+    borderColorChange(inputValue.target);
+}, true);
+
+formParent.addEventListener('keyup', function (inputValue) {
+    borderColorChange(inputValue.target);
+});
+
+
+formParent.addEventListener('change', function (inputValue) {
+    borderColorChange(inputValue.target);
+}, true);
+
+
+formParent.addEventListener('blur', function (inputValue) {
+    if (!isNaN(inputValue.target.value) && inputValue.target.value >= 0 || inputValue.target.value == '') {
+        inputValue.target.style.border = '1px solid ';
+        inputValue.target.style.outline = 'none';
+    }
+    else {
+        inputValue.target.style.border = '1px solid red';
+        inputValue.target.style.outline = 'none';
+    }
+}, true);
+
+
